6fca67063ee7f4a8c9d785aa75dfd9b85471b022
Manjeet Pardeshi <manjitpardeshi91991@gmail.com>
2024-08-10 14:56:45 +0530

    chore: small change

--- processing/utils.go
+++ processing/utils.go
@@ -4,9 +4,7 @@ func getRoomTracks(room roomData) []clientTrack {
 	tracks := make([]clientTrack, 0)
 
 	for _, c := range room.Clients {
-		for _, t := range c.Tracks {
-			tracks = append(tracks, t)
-		}
+		tracks = append(tracks, c.Tracks...)
 	}
 
 	return tracks
